8115e759f6abb52ade4767abeafc5feb8eea1266
Jedrzej Gasiorowski <klipen2003@gmail.com>
2022-03-23 11:05:20 +0100

    23.03.2022

---
+++ Repo do gita/Repo do gita/Repo do gita.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int lp = 0;
+	int p = 2;
+	int n;
+	int d;
+
+	cout << "Ile liczb ma byc ";
+	cin >> n;
+
+	while (lp < n)
+	{
+
+		for (int i = 2; i <= p -1; i++)
+		{
+			if (p % d = 0)
+			{
+				
+				
+			}
+			
+			
+		}
+
+
+	}
+
+	
+	
+
+	
+
+}
