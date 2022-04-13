fe5642fb1c6ed5971c1dabcece3df30317572d2c
Jedrzej Gasiorowski <klipen2003@gmail.com>
2022-04-13 11:01:04 +0200

    13.04.2022

---
+++ Zypher/Cypher/Cypher/Cypher.cpp
@@ -0,0 +1,66 @@
+﻿using namespace std;
+#include <iostream>
+
+void Funkcja1(tab[],klucz);
+
+int main()
+{
+    int klucz;
+    char tab[200];
+    int dl = strlen(tab);
+    
+
+
+
+    if (klucz < -26 && klucz > 26)
+    {
+
+
+    }
+    else
+    {
+        if (klucz)
+        {
+            for (int i = 0; i < dl; i++)
+            {
+                if (tab[i] <= 'z')
+                {
+
+                    tab[i] += klucz;
+
+                }
+                else
+                {
+                    tab[i] = tab[i] + klucz - 26;
+
+
+                }
+
+            }
+
+        }
+        else
+        {
+            for (int i = 0; i < dl; i++)
+            {
+
+
+                if (tab[i] + klucz >= 'a')
+                {
+
+                    tab[i] += klucz;
+                }
+                else
+                {
+                    tab[i] = tab[i] + klucz + 26;
+
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
