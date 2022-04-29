8a02154afaa64d589d232eb455bfd270a58b7c63
Jedrzej Gasiorowski <klipen2003@gmail.com>
2022-04-29 09:56:15 +0200

    29.4.2022

---
+++ Fibacośtam/Fibacośtam/Fibacośtam.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void fibonacci(int n);
+
+int main()
+{
+	int n;
+
+
+	cout << "Ile liczb?" << endl;
+	cin >> n;
+
+
+	fibonacci(n);
+
+}
+
+void fibonacci(int n)
+{
+
+	long long a = 0, b = 1;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << b << " ";
+		b += a;
+		a = b - a;
+
+	}
+}
